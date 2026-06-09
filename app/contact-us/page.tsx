7ca26b5b9057548c2883152b3585bc4e4788731e
jocelyn zheng <69088223+happired@users.noreply.github.com>
2026-06-09 10:17:44 -0400

    delete pages + contact

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <main className="main">
         <div style={{paddingBottom: "40px"}}>
             <h1>Contact Us</h1>
-            <p>Any inquiries can be directed to ____@gmail.com! Thank you!</p>
+            <p>Any inquiries can be directed to bbrootsandshoots@gmail.com! Thank you!</p>
         </div>
         
        
