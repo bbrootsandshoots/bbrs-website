daeb2eb388e8b236688139616aa304f1fd75a700
jocelyn zheng <69088223+happired@users.noreply.github.com>
2026-06-09 13:39:17 -0400

    remove some from navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -9,10 +9,10 @@ export default function Home() {
     <div className = "navbar-container">
             <ul className = "navbar">
                 <li><a href="/">Home</a></li>
-                <li><a href="/about-us">About Us</a></li>
+
                 <li><a href="/projects">Projects</a></li>
                 <li><a href="/interactive-map">Map</a></li>
-                <li><a href="/blog">Blog</a></li>
+                
                 <li><a href="/contact-us">Contact Us</a></li>
             </ul>
     </div>
